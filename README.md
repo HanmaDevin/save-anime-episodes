ce8b427dabbc4de42f00465ca1e2f4b850c60014
HanmaDevin <devinbc14@gmail.com>
2025-09-03 21:37:33 +0200

    updates

--- README.md
+++ README.md
@@ -4,9 +4,18 @@ This script is used to safe the watch history of anime episodes watched from ani
 It appends the episode to a text file named `watched_episodes.txt` in the `$HOME` directory. <br>
 Feel free to modify it to your needs.
 
+> [!NOTE]
+> There is also a log file created at `$HOME/.anime-watchlog.txt`
+
 # Usage
 
-Just after starting ani-cli run the script in antoher terminal.:
+1. Make the script executable:
+
+```bash
+chmod +x safe-anime-episode.sh
+```
+
+2. Just after starting ani-cli run the script in antoher terminal.:
 
 ```bash
 ./safe-anime-episode.sh
--- safe-anime-episode.sh
+++ safe-anime-episode.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 WATCHLIST="$HOME/anime-watchlist.txt"
+LOGFILE="$HOME/.anime-watchlog.txt"
 
 while true; do
   if ! pgrep -x "ani-cli" > /dev/null; then
@@ -9,11 +10,13 @@ while true; do
   watching=$(playerctl metadata xesam:title 2>/dev/null) # Get current title
   if [[ -n "$watching" ]]; then
     if ! grep -Fxq "$watching" "$WATCHLIST"; then # Check if not already in the list
+      echo "Safed: $watching"
+      echo "Safed: $watching" >> "$LOGFILE"
       echo "$watching" >> "$WATCHLIST"
-      notify-send "Anime" "Saved watch history"
     else
-      notify-send "Anime" "Already in watch history"
+      echo "Already in watchlist: $watching"
+      echo "Already in watchlist: $watching" >> "$LOGFILE"
     fi
   fi
-  sleep 900 # Check every 15 minutes
+  sleep 300 # Check every 5 minutes
 done
